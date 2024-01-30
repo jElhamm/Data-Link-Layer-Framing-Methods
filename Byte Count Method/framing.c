65e8d111355090ad973d1c25fc410bb25f287bdb
jelhamm <elhamjafari200312@gmail.com>
2024-01-31 01:19:09 +0330

    Function to remove byte count framing from the received data

--- Byte Count Method/framing.c
+++ Byte Count Method/framing.c
@@ -16,4 +16,16 @@ void addByteCountFraming(char* data, int dataLength, char* framedData) {
     framedData[1] = (char)((dataLength >> 8) & 0xFF);                                // Stores the most significant byte
     memcpy(framedData + 2, data, dataLength);                                        // Copies the data into framedData
 }
- 
+
+// Function to remove byte count framing from the received data
+int removeByteCountFraming(char* framedData, int framedDataLength, char* data) {
+    if (framedDataLength < 2) {
+        return -1;                                                                  // Error: Invalid frame length
+    }
+    int dataLength = (framedData[1] << 8) | framedData[0];                          // Retrieves the stored data length
+    if (dataLength > MAX_FRAME_SIZE - 2 || framedDataLength != dataLength + 2) {
+        return -1;                                                                  // Error: Invalid frame length or data length
+    }
+    memcpy(data, framedData + 2, dataLength);                                       // Copies the data into the data buffer
+    return dataLength;                                                              // Returns the length of the extracted data
+}
--- Byte Count Method/framing.h
+++ Byte Count Method/framing.h
@@ -7,6 +7,7 @@
 #define MAX_FRAME_SIZE 255
 
 void addByteCountFraming(char* data, int dataLength, char* framedData);
+int removeByteCountFraming(char* framedData, int framedDataLength, char* data);
 
 #endif
 
@@ -15,4 +16,9 @@ void addByteCountFraming(char* data, int dataLength, char* framedData);
   * 'addByteCountFraming: This function takes three parameters: a pointer to a character array (char* data), 
   * an integer (int dataLength) specifying the length of the data array, and another pointer to a character array (char* framedData). 
   * The function is responsible for adding byte count framing to the data array and storing the framed data in the framedData array.
- 
+
+  * 'removeByteCountFraming': This function takes three parameters: a pointer to a character array (char* framedData), 
+  * an integer (int framedDataLength) specifying the length of the framed data array, and another pointer to a character array (char* data). 
+  * The function is responsible for removing the byte count framing from the framed data array and storing the extracted data in the data array. 
+  * The function returns an integer representing the length of the extracted data.
+**/
