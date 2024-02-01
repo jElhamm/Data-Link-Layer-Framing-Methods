bbbecdf5eb6c25c147b9dd3833dc762ce2bbb400
jelhamm <elhamjafari200312@gmail.com>
2024-02-01 22:18:28 +0330

    Function to perform byte stuffing

---
+++ Flag Bytes with Byte Stuffing/byte_stuffing.c
@@ -0,0 +1,26 @@
+/** Byte stuffing is a process of adding an escape character before certain predefined bytes in a byte stream.
+  * In this code, the byteStuffing function takes an input byte array (input), its length (inputLen), and an output byte array (output). 
+  * It iterates over each byte in the input array and checks if it is equal to a FLAG_BYTE or ESCAPE_BYTE. 
+  * If it matches, it adds an ESCAPE_BYTE before the byte in the output array. Finally, it returns the length of the output array.
+  **/
+
+
+
+#include <stdio.h>
+#include "byte_stuffing.h"
+
+
+// Function to perform byte stuffing
+int byteStuffing(const char* input, int inputLen, char* output) {
+    int i, j;
+    j = 0;
+    for (i = 0; i < inputLen; i++) {
+        // If input byte is FLAG_BYTE or ESCAPE_BYTE, add an ESCAPE_BYTE before it
+        if (input[i] == FLAG_BYTE || input[i] == ESCAPE_BYTE) {
+            output[j++] = ESCAPE_BYTE;
+        }
+        output[j++] = input[i];
+    }
+    return j;
+}
+ 
---
+++ Flag Bytes with Byte Stuffing/byte_stuffing.h
@@ -0,0 +1,21 @@
+// This code is a header file (byte_stuffing.h) that defines the constants 
+// and function prototypes for byte stuffing and byte unstuffing.
+
+
+
+#ifndef BYTE_STUFFING_H
+#define BYTE_STUFFING_H
+#define FLAG_BYTE 0x7E
+#define ESCAPE_BYTE 0x7D
+
+int byteStuffing(const char* input, int inputLen, char* output);
+
+#endif
+
+
+
+/**
+  * The 'byteStuffing' function prototype declares a function that performs byte stuffing. 
+  * It takes an input byte array (input), its length (inputLen), and an output byte array (output).
+  * The function returns an integer representing the length of the output array.
+ 
